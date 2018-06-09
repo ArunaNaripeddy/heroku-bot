603f2a6977259a60ea32ed9a66f9797e53f55227
aruna <aruna.naripeddy@gmail.com>
2018-06-09 00:58:43 -0500

    first commit to add quotes to the home feed

--- ChatterBot.py
+++ ChatterBot.py
@@ -14,6 +14,26 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
 
+# Quotes to Tweet
+Quotes_list = ["Quote1", "Quote2", "Quote3", "Quote4"]
 
 # Create a function that tweets
-# CODE GOES HERE
+# Create function for tweeting
+def tweet_out(counter):
+    # Twitter Credentials
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
+    
+    # Tweet the next quote
+    api.update_status(Quotes_list[counter])
+
+    # Print success message
+    print("Tweeted Successfully")
+
+counter = 0
+# Set timer to run every minute
+while(counter < len(Quotes_list)):
+    tweet_out(counter)
+    time.sleep(10)
+    counter += 1
