9754e489cc6b319314c096dc25d0da15859b9bfd
aruna <aruna.naripeddy@gmail.com>
2018-06-09 01:07:17 -0500

    added config file

---
+++ config.py
@@ -0,0 +1,5 @@
+# These keys are not valid. Use your own API keys
+consumer_key = "2CSwQGA2BUaZ5F15C24XNLYax"
+consumer_secret = "kZ0CeB4tDfgEOunj1hj0848m2AqKoEpXPl3RgmQ6TpJOzzTN6s"
+access_token = "2565782251-cJ2kfV7R6VSxqCk5X2HuGkNcQElFVyQHed7S6n4"
+access_token_secret = "59pU4KslXdzw3HdEC4Iw2T7GQDMMP7v2WiISGiszPGlda"
